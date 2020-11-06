fb916bba23043da7025929004fa6d0528df470a3
Emma randle <emmarandle@Emmas-iMac.local>
2020-11-06 15:29:37 +0000

    BeckEmmaBranch - end of hackathon

--- src/Components/MarsWeather/index.js
+++ src/Components/MarsWeather/index.js
@@ -12,66 +12,110 @@ function MarsWeather() {
   const [marsPRE, setMarsPRE] = useState([]);
   const [marsAT, setMarsAT] = useState({});
 
-  useEffect(() => {
-    if (marsWeather) {
-      async function getPicture() {
-        const res = await fetch(
-          `https://api.nasa.gov/insight_weather/?api_key=DEMO_KEY&feedtype=json&ver=1.0`,
-          {
-            headers: { accept: "application/json" },
-          }
-        );
-        const data = await res.json();
-        console.log(data);
-        setMarsWeather(data);
-        console.log(data.sol_keys.length);
-       
-      }
-      getPicture();
-    }
-  }, []);
+    useEffect(() => {
+        if (marsWeather) {
+        async function getPicture() {
+            const res = await fetch(
+            `https://api.nasa.gov/insight_weather/?api_key=XN5CHF6pKgWR57EuktB9EvBhIetjqL7lAJmqBFXq&feedtype=json&ver=1.0`,
+            {
+                headers: { accept: "application/json" },
+            }
+            );
+            const data = await res.json();
+            setMarsWeather(data);
+        }
+        getPicture();
+        }
+    }, []);
 
     useEffect(() => {
-        if(marsWeather){
-    let pressureArray = []
-    for(let i = 0; i< marsWeather?.sol_keys?.length;i++){
-        let xd = marsWeather?.sol_keys[i];
-        let solDay = marsWeather?.sol_keys[i]
-        let yd = marsWeather[solDay].PRE?.av;
-        pressureArray.push({x: xd, y: yd})
-    }
-    console.log(pressureArray);
-    setMarsPRE([...pressureArray]);
-    console.log(marsPRE);
+            if(marsWeather){
+        let pressureArray = []
+        for(let i = 0; i< marsWeather?.sol_keys?.length;i++){
+            let xd = marsWeather?.sol_keys[i];
+            let solDay = marsWeather?.sol_keys[i]
+            let yd = marsWeather[solDay].PRE?.av;
+            pressureArray.push({x: xd, y: yd})
+        }
 
-}
-}, [marsWeather])
+        setMarsPRE([...pressureArray]);
+        }
+        }, [marsWeather])
+
+    useEffect(() => {
+            if(marsWeather){
+        let tempArray = []
+        for(let i = 0; i< marsWeather?.sol_keys?.length;i++){
+            if(i === 1 || i === 2){
+                continue;
+            }
+            let xd = marsWeather?.sol_keys[i];
+            let solDay = marsWeather?.sol_keys[i]
+            let yd = marsWeather[solDay].AT?.av;
+            tempArray.push({x: xd, y: yd})
+        }
+        setMarsAT([...tempArray]);
+        console.log(marsAT);
+
+        }
+        }, [marsWeather])
 
   return (
     <div>
+    <h1>Under Pressure on Mars </h1>
+    <img src={`https://images-na.ssl-images-amazon.com/images/I/71NrROgheSL._AC_SX679_.jpg`} width={300} height={`auto`}/>
+    <br></br>
+    <container className='graphs'>
         <XYPlot
-            width={300}
-            height={300}>
-        <HorizontalGridLines style={{stroke: '#B7E9ED'}} />
-        <VerticalGridLines style={{stroke: '#B7E9ED'}} />
+            width={800}
+            height={500}>
+        <HorizontalGridLines style={{stroke: 'white'}} />
+        <VerticalGridLines style={{stroke: 'white'}} />
         <XAxis
-            title="XMars Sol - Solar Days"
+            title="Mars Sol - Solar Days"
             style={{
-            line: {stroke: '#ADDDE1'},
-            ticks: {stroke: '#ADDDE1'},
-            text: {stroke: 'none', fill: '#6b6b76', fontWeight: 600}
+            line: {stroke: 'white'},
+            ticks: {stroke: 'white'},
+            text: {stroke: 'none', fill: 'white', fontWeight: 600}
             }}
         />
         <YAxis title="PRE: Pressure Pa" />
         <LineSeries
+
             className="first-series"
             data={marsPRE}
             style={{
+                color: `white`,
+                fill: 'none',
                 strokeLinejoin: 'round',
                 strokeWidth: 4}}
-
         />
-    </XYPlot>
+        </XYPlot>
+        <br></br>
+        {/* <XYPlot
+                width={300}
+                height={300}>
+            <HorizontalGridLines style={{stroke: '#B7E9ED'}} />
+            <VerticalGridLines style={{stroke: '#B7E9ED'}} />
+            <XAxis
+                title="Mars Sol - Solar Days"
+                style={{
+                line: {stroke: '#ADDDE1'},
+                ticks: {stroke: '#ADDDE1'},
+                text: {stroke: 'none', fill: '#6b6b76', fontWeight: 600}
+                }}
+            />
+            <YAxis title="AT: Average Temperature" />
+            <LineSeries
+                className="second-series"
+                data={marsAT}
+                style={{
+                    strokeLinejoin: 'round',
+                    strokeWidth: 4}}
+
+            />
+        </XYPlot> */}
+    </container>
     </div>
   );
 }
