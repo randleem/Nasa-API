a93544ba6af5f71b633a31e06763303a4d311866
= <beckyljones151192@gmail.com>
2020-11-06 15:52:10 +0000

    put react routes on

--- src/App.js
+++ src/App.js
@@ -3,13 +3,29 @@ import "./App.css";
 import React from "react";
 import MarsWeather from "./Components/MarsWeather/index";
 import Calendar from "./Components/Calendar/index";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Calendar />
-      <MarsWeather/>
-    </div>
+    <Router>
+      <div className="App">
+        <nav>
+          <Link to="/">Space Birthday Picture</Link>
+          <Link to="/marsPressure">Mars Pressured by Galaxy</Link>
+        </nav>
+        <Switch>
+          <Route path="/marsPressure">
+            <MarsWeather />
+          </Route>
+          <Route path="/">
+            <Calendar />
+          </Route>
+          {/* <Route path="/">
+            <Home />
+          </Route> */}
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
--- src/Components/MarsWeather/index.js
+++ src/Components/MarsWeather/index.js
@@ -1,95 +1,98 @@
 import React, { useEffect, useState } from "react";
-import {curveCatmullRom} from 'd3-shape';
-import {XYPlot,
-XAxis,
-YAxis,
-HorizontalGridLines,
-VerticalGridLines,
-LineSeries, LineMarkSeries} from 'react-vis';
+import {
+  XYPlot,
+  XAxis,
+  YAxis,
+  HorizontalGridLines,
+  VerticalGridLines,
+  LineSeries,
+  LineMarkSeries,
+} from "react-vis";
 
 function MarsWeather() {
   const [marsWeather, setMarsWeather] = useState({});
   const [marsPRE, setMarsPRE] = useState([]);
   const [marsAT, setMarsAT] = useState({});
 
-    useEffect(() => {
-        if (marsWeather) {
-        async function getPicture() {
-            const res = await fetch(
-            `https://api.nasa.gov/insight_weather/?api_key=XN5CHF6pKgWR57EuktB9EvBhIetjqL7lAJmqBFXq&feedtype=json&ver=1.0`,
-            {
-                headers: { accept: "application/json" },
-            }
-            );
-            const data = await res.json();
-            setMarsWeather(data);
-        }
-        getPicture();
-        }
-    }, []);
+  useEffect(() => {
+    if (marsWeather) {
+      async function getPicture() {
+        const res = await fetch(
+          `https://api.nasa.gov/insight_weather/?api_key=XN5CHF6pKgWR57EuktB9EvBhIetjqL7lAJmqBFXq&feedtype=json&ver=1.0`,
+          {
+            headers: { accept: "application/json" },
+          }
+        );
+        const data = await res.json();
+        setMarsWeather(data);
+      }
+      getPicture();
+    }
+  }, []);
 
-    useEffect(() => {
-            if(marsWeather){
-        let pressureArray = []
-        for(let i = 0; i< marsWeather?.sol_keys?.length;i++){
-            let xd = marsWeather?.sol_keys[i];
-            let solDay = marsWeather?.sol_keys[i]
-            let yd = marsWeather[solDay].PRE?.av;
-            pressureArray.push({x: xd, y: yd})
-        }
+  useEffect(() => {
+    if (marsWeather) {
+      let pressureArray = [];
+      for (let i = 0; i < marsWeather?.sol_keys?.length; i++) {
+        let xd = marsWeather?.sol_keys[i];
+        let solDay = marsWeather?.sol_keys[i];
+        let yd = marsWeather[solDay].PRE?.av;
+        pressureArray.push({ x: xd, y: yd });
+      }
 
-        setMarsPRE([...pressureArray]);
-        }
-        }, [marsWeather])
+      setMarsPRE([...pressureArray]);
+    }
+  }, [marsWeather]);
 
-    useEffect(() => {
-            if(marsWeather){
-        let tempArray = []
-        for(let i = 0; i< marsWeather?.sol_keys?.length;i++){
-            if(i === 1 || i === 2){
-                continue;
-            }
-            let xd = marsWeather?.sol_keys[i];
-            let solDay = marsWeather?.sol_keys[i]
-            let yd = marsWeather[solDay].AT?.av;
-            tempArray.push({x: xd, y: yd})
+  useEffect(() => {
+    if (marsWeather) {
+      let tempArray = [];
+      for (let i = 0; i < marsWeather?.sol_keys?.length; i++) {
+        if (i === 1 || i === 2) {
+          continue;
         }
-        setMarsAT([...tempArray]);
-        console.log(marsAT);
-
-        }
-        }, [marsWeather])
+        let xd = marsWeather?.sol_keys[i];
+        let solDay = marsWeather?.sol_keys[i];
+        let yd = marsWeather[solDay].AT?.av;
+        tempArray.push({ x: xd, y: yd });
+      }
+      setMarsAT([...tempArray]);
+      console.log(marsAT);
+    }
+  }, [marsWeather]);
 
   return (
     <div>
-    <h1>Under Pressure on Mars </h1>
-    <img src={`https://images-na.ssl-images-amazon.com/images/I/71NrROgheSL._AC_SX679_.jpg`} width={300} height={`auto`}/>
-    <br></br>
-    <container className='graphs'>
-        <XYPlot
-            width={800}
-            height={500}>
-        <HorizontalGridLines style={{stroke: 'white'}} />
-        <VerticalGridLines style={{stroke: 'white'}} />
-        <XAxis
+      <h1>Under Pressure on Mars </h1>
+      <img
+        src={`https://images-na.ssl-images-amazon.com/images/I/71NrROgheSL._AC_SX679_.jpg`}
+        width={300}
+        height={`auto`}
+      />
+      <br></br>
+      <container className="graphs">
+        <XYPlot width={800} height={500}>
+          <HorizontalGridLines style={{ stroke: "white" }} />
+          <VerticalGridLines style={{ stroke: "white" }} />
+          <XAxis
             title="Mars Sol - Solar Days"
             style={{
-            line: {stroke: 'white'},
-            ticks: {stroke: 'white'},
-            text: {stroke: 'none', fill: 'white', fontWeight: 600}
+              line: { stroke: "white" },
+              ticks: { stroke: "white" },
+              text: { stroke: "none", fill: "white", fontWeight: 600 },
             }}
-        />
-        <YAxis title="PRE: Pressure Pa" />
-        <LineSeries
-
+          />
+          <YAxis title="PRE: Pressure Pa" />
+          <LineSeries
             className="first-series"
             data={marsPRE}
             style={{
-                color: `white`,
-                fill: 'none',
-                strokeLinejoin: 'round',
-                strokeWidth: 4}}
-        />
+              color: `white`,
+              fill: "none",
+              strokeLinejoin: "round",
+              strokeWidth: 4,
+            }}
+          />
         </XYPlot>
         <br></br>
         {/* <XYPlot
@@ -115,7 +118,7 @@ function MarsWeather() {
 
             />
         </XYPlot> */}
-    </container>
+      </container>
     </div>
   );
 }
